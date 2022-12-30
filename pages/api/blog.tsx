262476fe44b3d5a1e310f5aa4a914ac02c992ab5
Brendan Allan <brendonovich@outlook.com>
2022-12-31 06:01:44 +0800

    add blog og

---
+++ pages/api/blog.tsx
@@ -0,0 +1,69 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+const getFont = async (url: URL) => {
+  const res = await fetch(url);
+  return await res.arrayBuffer();
+};
+
+export default async function handler(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  const date = searchParams.get("date");
+  const readTime = searchParams.get("readTime");
+  const title = searchParams.get("title");
+
+  return new ImageResponse(
+    (
+      <div tw="w-full h-full pl-24 pt-20 pr-20 pb-20 flex flex-col items-start justify-between bg-neutral-900 text-white">
+        <div tw="flex-1 text-3xl flex flex-row w-full">
+          <div tw="w-full flex flex-row items-center">
+            <span>{date}</span>
+            <span tw="ml-16">{readTime}</span>
+            <div tw="flex-1" />
+            <img
+              alt="avatar"
+              width={8 * 12}
+              src={`https://github.com/brendonovich.png`}
+              tw="rounded-full"
+            />
+          </div>
+        </div>
+        <div tw="flex-1 text-7xl text-left">{title}</div>
+        <div tw="flex-1 w-full flex items-end justify-between flex-row">
+          <span tw="text-yellow-400 text-5xl">Brendan</span>
+          <span
+            tw="mb-1 text-3xl text-neutral-300"
+            style={{ fontFamily: "InterSemibold" }}
+          >
+            brendonovich.dev
+          </span>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "InterBlack",
+          data: await getFont(
+            new URL(`../../assets/Inter-Black.ttf`, import.meta.url)
+          ),
+          style: "normal",
+        },
+        {
+          name: "InterSemibold",
+          data: await getFont(
+            new URL(`../../assets/Inter-Semibold.ttf`, import.meta.url)
+          ),
+          style: "normal",
+        },
+      ],
+    }
+  );
+}
